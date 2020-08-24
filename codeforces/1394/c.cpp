64394ea51d06ce21f472e4a2643fff634da3baab
Timothy <t.d.horscroft@gmail.com>
2020-08-24 13:20:19 +1000

    Create c.cpp

---
+++ codeforces/1394/c.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define dub long double
+#define FOR(i, a, b) for (int i=(a); i<(b); ++i)
+#define ROF(i, a, b) for (int i=(a); i>(b); --i)
+#define CASET int _T; cin >> _T; FOR(caset, 1, _T+1)
+#define pii pair<int,int>
+#define t3i tuple<int,int,int>
+#define mii map<int,int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvb vector<vb>
+#define vpii vector<pii>
+#define vt3i vector<t3i>
+#define vvpii vector<vpii>
+#define sq(x) (x)*(x)
+#define inv(x) powa(x, MOD-2)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SIZE(x) (int)(x).size()
+#define SUM(x) accumulate(ALL(x), 0LL)
+ 
+long long INF = 1LL<<60;
+long long MOD = 1e9 + 7;
+ 
+namespace output {
+    void pr(short x) { cout << x; }
+    void pr(signed x) { cout << x; }
+    void pr(long x) { cout << x; }
+    void pr(long long x) { cout << x; }
+    void pr(unsigned short x) { cout << x; }
+    void pr(unsigned x) { cout << x; }
+    void pr(unsigned long x) { cout << x; }
+    void pr(unsigned long long x) { cout << x; }
+    void pr(float x) { cout << x; }
+    void pr(double x) { cout << x; }
+    void pr(long double x) { cout << x; }
+    void pr(char x) { cout << x; }
+    void pr(const char* x) { cout << x; }
+    void pr(const string& x) { cout << x; }
+    void pr(bool x) { cout << x; }
+    template<size_t sz> void pr(const bitset<sz>& x) { cout << x; }
+    template<class T> void pr(const complex<T>& x) { cout << x; }
+    
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T1, class T2, class T3> void pr(const tuple<T1,T2,T3>& x);
+    template<class T> void pr(const T& x);
+    
+    template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { pr("(",x.first,", ",x.second,")"); }
+    template<class T1, class T2, class T3> void pr(const tuple<T1,T2,T3>& x) { pr("(",get<0>(x),", ",get<1>(x),", ",get<2>(x),")"); }
+    template<class T> void pr(const T& x) { 
+        pr("["); // const iterator needed for vector<bool>
+        bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+        pr("]");
+    }
+    
+    void print() { pr("\n"); } // print w/ spaces
+    template<class T, class... Ts> void print(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(" "); print(ts...); }
+}
+using namespace output;
+ 
+template <class T1, class T2> istream &operator>>(istream &is, pair<T1,T2> &x) { is >> x.first >> x.second; return is; }
+template <class T1, class T2, class T3> istream &operator>>(istream &is, tuple<T1,T2,T3> &x) { is >> get<0>(x) >> get<1>(x) >> get<2>(x); return is; }
+template <class T> istream &operator>>(istream &is, vector<T> &v) { for (auto &x : v) is >> x; return is; }
+int powa(int base, int exp) {
+    int res = 1;
+    while (exp) {
+        if (exp&1) res = res*base % MOD;
+        base = base*base % MOD;
+        exp >>= 1;
+    }
+    return res;
+}
+bool mini(int &a, int b) { return b < a ? a = b, 1 : 0; }
+bool maxi(int &a, int b) { return b > a ? a = b, 1 : 0; }
+ 
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    int n;
+    cin >> n;
+
+    int l0=-INF, r0=INF, d0=-INF, u0=INF, e0=-INF, f0=INF;
+    FOR(i, 0, n) {
+        string s;
+        cin >> s;
+        int x=0, y=0;
+        for (char c : s) {
+            if (c == 'B') ++x;
+            else ++y;
+        }
+        maxi(l0, x);
+        mini(r0, x);
+        maxi(d0, y);
+        mini(u0, y);
+        maxi(e0, x-y);
+        mini(f0, x-y);
+    }
+
+    int k = -1;
+    int upp = 1e7;
+    for (int b=upp; b>=1; b/=2) {
+        while (k+b <= upp) {
+            k += b;
+            int l=l0-k, r=r0+k, d=d0-k, u=u0+k, e=e0-k, f=f0+k;
+            maxi(l, 0);
+            maxi(d, 0);
+            if (l <= r && d <= u && e <= f && l-u <= r-d && !(r-d < e || f < l-u)) {
+                k -= b;
+                break;
+            }
+        }
+    }
+    ++k;
+    print(k);
+
+    int l=l0-k, r=r0+k, d=d0-k, u=u0+k, e=e0-k, f=f0+k;
+    maxi(l, 0);
+    maxi(d, 0);
+    int x = l, y = u;
+    int diff = max(l-u, e);
+    x += diff - (x-y);
+    if (x > r) {
+        y -= x-r;
+        x = r;
+    }
+    print(string(x, 'B') + string(y, 'N'));
+}
