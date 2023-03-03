5511d7baec38d4ec02f60d839e9e5968582acba1
Timothy Horscroft <t.d.horscroft@gmail.com>
2023-03-03 17:34:40 +1100

    solved more problems

--- codeforces/1795/c.cpp
+++ codeforces/1795/c.cpp
@@ -22,11 +22,20 @@ signed main() {
         vi bpref = b;
         FOR(i, 1, n) bpref[i] += bpref[i-1];
 
-        vector<pii> x(n);
-
+        vi resfree(n), res(n);
         FOR(i, 0, n) {
-            x[i].first = lower_bound(bpref.begin(), bpref.end(), a[i] + bpref[i-1]) - bpref.begin() - i;
-            x[i].second = bpref
+            int prev = i == 0 ? 0 : bpref[i-1];
+            int x = upper_bound(bpref.begin(), bpref.end(), prev+a[i]) - bpref.begin();
+            ++resfree[i];
+            if (x < n) {
+                --resfree[x];
+                res[x] += prev+a[i] - (x==0 ? 0 : bpref[x-1]);
+            }
         }
+        FOR(i, 1, n) resfree[i] += resfree[i-1];
+        FOR(i, 0, n) res[i] += b[i] * resfree[i];
+
+        FOR(i, 0, n) cout << res[i] << ' ';
+        cout << '\n';
     }
 }
